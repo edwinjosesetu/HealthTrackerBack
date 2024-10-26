cf1280c09ef3e8b13ffef89eeba829893df97c8e
Edwin Jose <mjedwin17@gmail.com>
2024-10-26 12:47:37 +0100

    Initial push - Wrote basic api for lab 4

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -1,4 +1,28 @@
 package ie.setu.config
 
+import ie.setu.controllers.HealthTrackerController
+import io.javalin.Javalin
+import io.javalin.apibuilder.ApiBuilder.*
+
 class JavalinConfig {
+
+    fun startJavalinService(): Javalin {
+
+        val app = Javalin.create().apply {
+            exception(Exception::class.java) { e, ctx -> e.printStackTrace() }
+            error(404) { ctx -> ctx.json("404 - Not Found") }
+        }.start(7001)
+
+        registerRoutes(app)
+        return app
+    }
+
+    private fun registerRoutes(app: Javalin) {
+//        get(HealthTrackerController::getAllUsers)
+        app.get("/api/users", HealthTrackerController::getAllUsers)
+        app.get("/api/users/{user-id}", HealthTrackerController::getUserByUserId)
+        app.post("/api/users/create-user", HealthTrackerController::addUser)
+        app.get("/api/users/email/{email}", HealthTrackerController::getUserByEmail)
+        app.delete("/api/users/remove-user/{user-id}", HealthTrackerController::deleteUserById)
+    }
 }
--- src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
+++ src/main/kotlin/ie/setu/controllers/HealthTrackerController.kt
@@ -1,4 +1,39 @@
 package ie.setu.controllers
+import ie.setu.domain.repository.UserDAO
+import io.javalin.http.Context
 
-class HealthTrackerController {
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.readValue
+
+import ie.setu.domain.User
+
+object HealthTrackerController {
+
+    private val userDao = UserDAO()
+
+    fun getAllUsers(ctx: Context) {
+        ctx.json(userDao.getAll())
+    }
+
+    fun getUserByUserId(ctx: Context) {
+        val user = userDao.findById(ctx.pathParam("user-id").toInt())
+        if (user != null) {
+            ctx.json(user)
+        }
+    }
+    fun addUser(ctx: Context) {
+        val mapper = jacksonObjectMapper()
+        val user = mapper.readValue<User>(ctx.body())
+        userDao.save(user)
+        ctx.json(user)
+    }
+    fun getUserByEmail(ctx: Context) {
+        val email = userDao.findByEmail(ctx.pathParam("email"))
+        if (email != null) {
+            ctx.json(email)
+        }
+    }
+    fun deleteUserById(ctx: Context) {
+         userDao.deleteUserById(ctx.pathParam("user-id").toInt())
+    }
 }
