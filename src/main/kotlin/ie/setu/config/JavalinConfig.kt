28881961ef3a199aa07ce28e3b9ef1e537520ebc
Edwin Jose <mjedwin17@gmail.com>
2024-12-27 23:10:07 +0000

    Add CRUD for activity front page

--- src/main/kotlin/ie/setu/config/JavalinConfig.kt
+++ src/main/kotlin/ie/setu/config/JavalinConfig.kt
@@ -83,7 +83,7 @@ class JavalinConfig {
         app.get("/", VueComponent("<home-page></home-page>"))
         app.get("/users", VueComponent("<user-overview></user-overview>"))
         app.get("/users/{user-id}", VueComponent("<user-profile></user-profile>"))
-        app.get("/users/{user-id}/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
+        app.get("/activities", VueComponent("<user-activity-overview></user-activity-overview>"))
 
     }
     private fun getRemoteAssignedPort(): Int {
